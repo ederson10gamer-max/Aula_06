b0adbb44e293d1c517d77240ce91cd4f3fd5e91c
Edson <ederson10gamer@gmail.com>
2026-09-23 12:10:07 -0300

    Ajusta pastas clonadas e adiciona arquivos do projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+// Seleciona o botão e a barra lateral do HTML usando os IDs
+const btnToggle = document.getElementById('btn-mostrar');
+const sidebar = document.getElementById('sidebar');
+
+// Adiciona o evento de clique ao botão
+btnToggle.addEventListener('click', () => {
+   
+    sidebar.classList.toggle('oculto');
+});
